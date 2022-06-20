b4f4701406c99afbcfea29cccadf0ed9a734fe34
O9BitsO <88641359+O9BitsO@users.noreply.github.com>
2022-06-20 09:48:51 +0000

    movement

---
+++ index.js
@@ -0,0 +1,114 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.2;
+
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, 150);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
+  }
+}
+
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+player.draw();
+enemy.draw();
+
+const keys = {
+    a: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    },
+    w: {
+        pressed: false,
+    },
+    s: {
+        pressed: false,
+    },
+}
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+
+  player.velocity.x = 0;
+  if(keys.a.pressed) {
+    player.velocity.x = -1;
+  } else if(keys.d.pressed) {
+    player.velocity.x = 1;
+  }
+}
+
+animate();
+
+window.addEventListener("keydown", function(e) {
+    switch (e.key) {
+        case 'd':
+            keys.d.pressed = true;
+            break;
+        case 'a':
+            keys.a.pressed = true;
+            break;
+    }
+});
+
+window.addEventListener("keyup", function(e) {
+    switch (e.key) {
+        case 'd':
+            keys.d.pressed = false;
+            break;
+        case 'a':
+            keys.a.pressed = false;
+            break;
+    }
+});
+
