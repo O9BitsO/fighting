844f02ef1bffd1a4410fc66c184227cdde047776
O9BitsO <88641359+O9BitsO@users.noreply.github.com>
2022-06-21 04:32:52 +0000

    basic

--- index.js
+++ index.js
@@ -9,19 +9,24 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.7;
 
 class Sprite {
-  constructor({ position, velocity, color }) {
+  constructor({ position, velocity, color, offset }) {
     this.position = position;
     this.velocity = velocity;
     this.height = 150;
     this.width = 50;
     this.lastKey;
     this.attackBox = {
-      position: this.position,
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      offset,
       width: 100,
       height: 50,
     };
     this.color = color;
     this.isAttacking;
+    this.health = 100;
   }
 
   draw() {
@@ -42,6 +47,8 @@ class Sprite {
 
   update() {
     this.draw();
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y + this.attackBox.offset.y;
     this.position.y += this.velocity.y;
     this.position.x += this.velocity.x;
 
@@ -70,6 +77,10 @@ const player = new Sprite({
     y: 0,
   },
   color: "red",
+  offset: {
+    x: 0,
+    y: 0,
+  },
 });
 
 const enemy = new Sprite({
@@ -82,6 +93,10 @@ const enemy = new Sprite({
     y: 0,
   },
   color: "blue",
+  offset: {
+    x: -50,
+    y: 0,
+  },
 });
 
 player.draw();
@@ -114,6 +129,48 @@ const keys = {
   },
 };
 
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+      rectangle2.position.x &&
+    rectangle1.attackBox.position.x <=
+      rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+      rectangle2.position.y &&
+    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+  );
+}
+
+function determineWinner({ player, enemy, timerId }) {
+  clearTimeout(timerId);
+  if (player.health === enemy.health) {
+    document.querySelector("#result").innerHTML = "Tie";
+    document.querySelector("#result").style.display = "flex";
+  }
+  if (player.health > enemy.health) {
+    document.querySelector("#result").innerHTML = "Player 1 Win";
+    document.querySelector("#result").style.display = "flex";
+  }
+  if (player.health < enemy.health) {
+    document.querySelector("#result").innerHTML = "Player 2 Win";
+    document.querySelector("#result").style.display = "flex";
+  }
+}
+
+let timer = 10;
+let timerId;
+function decreaseTimer() {
+  if (timer > 0) {
+    timerId = setTimeout(decreaseTimer, 1000);
+    timer -= 1;
+    document.querySelector("#timer").innerHTML = timer;
+  }
+  if (timer === 0) {
+    determineWinner({player, enemy, timerId});
+  }
+}
+decreaseTimer();
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
@@ -139,15 +196,27 @@ function animate() {
 
   // Colision detection
   if (
-    player.attackBox.position.x + player.attackBox.width >= enemy.position.x &&
-    player.attackBox.position.x <= enemy.position.x + enemy.width &&
-    player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
-    player.attackBox.position.y <= enemy.position.y + enemy.height &&
+    rectangularCollision({ rectangle1: player, rectangle2: enemy }) &&
     player.isAttacking
   ) {
     player.isAttacking = false;
     enemy.color = "green";
+    enemy.health -= 10;
+    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
   } else enemy.color = "blue";
+
+  if (
+    rectangularCollision({ rectangle1: enemy, rectangle2: player }) &&
+    enemy.isAttacking
+  ) {
+    enemy.isAttacking = false;
+    player.color = "green";
+    player.health -= 10;
+    document.querySelector("#playerHealth").style.width = player.health + "%";
+  } else player.color = "red";
+
+  // end game
+  if(enemy.health <= 0 || player.health <= 0) determineWinner({player, enemy, timerId});
 }
 
 animate();
@@ -184,7 +253,8 @@ window.addEventListener("keydown", function (e) {
       enemy.velocity.y = -15;
       break;
     case "ArrowDown":
-      enemy.velocity.y = 5;
+      // enemy.velocity.y = 5;
+      enemy.attack();
       break;
   }
 });
